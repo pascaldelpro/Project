4622ab032db73a3810e04a679f0e398a09940438
pascaldelpro <pascaldelpro@gmail.com>
2024-03-07 23:28:18 +0330

    2.5.1.1

--- login.php
+++ login.php
@@ -84,6 +84,15 @@ body{
     justify-content: right;
     align-items: center;
 }
+
+.InputText{
+    width: 300px;
+    height: 30px;
+    border-radius: 12px;
+    padding-left: 7px;
+    padding-right: 7px;
+    margin-left: 30px;
+}
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -111,11 +120,12 @@ body{
         </tr>
         <tr>
             <td>
-            <form action="actionLogin.php" method="post" name="">
-                username<input type="text" name="username">
+            <form action="actionLogin.php" method="post" style="width: 49%;">
+                <h4 style="float: left;">username</h4><input type="text" name="username" class="InputText">
                 <br>
-                password<input type="password" name="password">
+                <h4 style="float: left;">password</h4><input type="password" name="password"  class="InputText">
                 <br>
+                <a href="Signin.php" style="text-decoration: none; color: orange; font-size: large;">Register</a>
                 <input type="submit">
             </form>
             </td>
